c4a13334fa46fc52bbadb77f53fe4ac5649acf95
Grig <chiefy@mail.ru>
2022-08-29 23:49:48 +0300

    Example 007 Magic

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(4, 10);
+//Console.WriteLine("+");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
