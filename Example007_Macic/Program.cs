4e31c48831e88e3f28340a86e1a9596680c82480
Grig <chiefy@mail.ru>
2022-08-30 21:07:58 +0300

    Add example 007 Magic final save

--- Example007_Macic/Program.cs
+++ Example007_Macic/Program.cs
@@ -2,9 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 40, ya = 1,
-    xb = 1, yb = 20,
-    xc = 80, yc = 20;
+int xa = 35, ya = 1,
+    xb = 1, yb = 16,
+    xc = 70, yc = 16;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
